c38cfb248b8e792409970a75b5b56248ab942277
Vitalii <example@gmail.com>
2024-11-18 18:59:16 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -16,9 +16,17 @@ const toggleTexts = (texts, showFirst) => {
         texts[1].style.transform = showFirst ? 'translateY(150%)' : 'translateY(0)';
     }
 };
+
+
+
+document.querySelectorAll('.clients__block-wrap').forEach(text => {
+    const clientText = text.querySelectorAll('.clients__text-wrap');
+    text.addEventListener('mouseenter', () => {  });
+});
+
 document.querySelectorAll('[data-hover-elem]').forEach(elem => {
     const texts = elem.querySelectorAll('.button__text');
-    
+
     elem.addEventListener('mouseenter', () => { toggleTexts(texts, false) });
     elem.addEventListener('mouseleave', () => { toggleTexts(texts, true) });
 });
